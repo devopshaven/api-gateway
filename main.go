15f05fdab08603d9ab88e0b23ba21e2f7b82e0d0
Gyula Paal <paalgyula@paalgyula.com>
2022-04-20 01:56:19 +0200

    Added graceful shutdown and -pretty flag for deveoper friendly logs

--- main.go
+++ main.go
@@ -1,21 +1,77 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.pirat.app/api-gateway/gateway"
 )
 
+var (
+	addr              *string
+	authServerAddress *string
+	prettyLog         *bool
+)
+
+func init() {
+	addr = flag.String("addr", "127.0.0.1:8080", "The addr of the application.")
+	authServerAddress = flag.String("authServer", "", "authorization server address")
+	prettyLog = flag.Bool("pretty", false, "use developer friendly log")
+}
+
 func main() {
-	var addr = flag.String("addr", "127.0.0.1:8080", "The addr of the application.")
 	flag.Parse()
 
-	handler := gateway.NewGateway()
-
-	log.Info().Msgf("Starting proxy server on %s", *addr)
-	if err := http.ListenAndServe(*addr, handler); err != nil {
-		log.Fatal().Err(err).Msgf("ListenAndServe: %s", err.Error())
+	if *prettyLog {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout}).With().Caller().Logger()
 	}
+
+	gateway := gateway.NewGateway(*authServerAddress)
+
+	otelHandler := otelhttp.NewHandler(gateway, "gateway")
+
+	srv := &http.Server{
+		Addr:    *addr,
+		Handler: otelHandler,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Msgf("listen: %s\n", err)
+		}
+	}()
+
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+
+	log.Info().Msgf("Server is running with %d goroutines with allocated heap: %0.2fMB",
+		runtime.NumGoroutine(),
+		float64(ms.TotalAlloc)/1024/1024,
+	)
+
+	// Wait for terminate/interrupt signals
+	<-done
+
+	gateway.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal().Msgf("Server Shutdown Failed: %+v", err)
+	}
+
+	log.Info().Msg("Server Exited Properly")
 }
